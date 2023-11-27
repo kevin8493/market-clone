fb2f0ebc2bca41319e02cd222c225bca17d521e6
kevin8493 <114860013+kevin8493@users.noreply.github.com>
2023-11-27 17:21:50 +0900

    이미지가 안올라올경우 명시해줌

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ con = sqlite3.connect('db.db',check_same_thread=False)
 cur = con.cursor()
 
 cur.execute(f"""
-            CREATE TABLE items (
+            CREATE TABLE IF NOT EXISTS items (
 	            id INTEGER PRIMARY KEY,
 	            title TEXT NOT NULL,
 	            image BLOB,
@@ -54,7 +54,7 @@ async def get_image(item_id):
     image_bytes = cur.execute(f"""
                               SELECT image from items WHERE id={item_id}
                               """).fetchone()[0]
-    return Response(content=bytes.fromhex(image_bytes))
+    return Response(content=bytes.fromhex(image_bytes), media_type='image/*')
 
 app.mount("/", StaticFiles(directory="frontend", html = True), name="frontend")
 
