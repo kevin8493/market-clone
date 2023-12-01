acf4a69c763288a1fe58255be6c14ac0e6a0f15e
kevin8493 <114860013+kevin8493@users.noreply.github.com>
2023-12-01 21:57:55 +0900

    회원가입 로직 기능 구현
    프론트엔드 및 백엔드 작업(signup)

--- frontend/signup.js
+++ frontend/signup.js
@@ -24,9 +24,11 @@ const handleSubmit = async (event) => {
       body: formData,
     });
     const data = await res.json();
+
     if (data === "200") {
       div.innerText = "회원가입에 성공했습니다";
       div.style.color = "blue";
+      window.location.pathname = "/login.html";
     }
   } else {
     div.innerText = "비밀번호가 같지 않습니다.";
