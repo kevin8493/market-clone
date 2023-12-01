f27aeed99ea3fe471bb02531f2e4af348503bae8
kevin8493 <114860013+kevin8493@users.noreply.github.com>
2023-12-02 02:42:02 +0900

    svelte에 적용

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Login from "./pages/Login.svelte";
+  import Main from "./pages/Main.svelte";
+  import Signup from "./pages/Signup.svelte";
+  import Write from "./pages/Write.svelte";
+  import Router from "svelte-spa-router";
+  import NotFound from "./pages/NotFound.svelte";
+  import "./css/style.css";
+
+  const routes = {
+    "/": Main,
+    "/login": Login,
+    "/signup": Signup,
+    "/write": Write,
+    "*": NotFound,
+  };
+</script>
+
+<Router {routes} />
+<main>
+  <div></div>
+</main>
+
+<style>
+</style>
